fd7b525436cc1f6a70ad25d0f44edad7918534dc
cristiandontu <cristian.dontu@isa.utm.md>
2026-09-14 14:39:56 +0300

    Buttons on left

--- my-react/src/App.jsx
+++ my-react/src/App.jsx
@@ -1,9 +1,11 @@
 import MainMinigamePage from "./MainMinigamePage.jsx";
+import Button1 from "./Buttons/Buttons.jsx";
 
 function App() {
 
   return(
     <>
+      <Button1/>
       <MainMinigamePage/>
     </>
   );
