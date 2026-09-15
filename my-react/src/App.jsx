0f14d7ba1d448af1fd915f2c6458d4c85f4346b4
cristiandontu <cristian.dontu@isa.utm.md>
2026-09-15 11:01:42 +0300

    Base UI part1

--- my-react/src/App.jsx
+++ my-react/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import MainMinigamePage from "./MainMinigamePage.jsx";
 import ButtonKT from "./Buttons/Buttons.jsx";
+import Button1 from "./Buttons/Buttons.jsx";
+import All from "./Base/All.jsx";
 
 function App() {
   const [isTeacher , setIsTeacher] = useState(true);
@@ -8,6 +10,9 @@ function App() {
     <>
       <ButtonKT isTeacher= {isTeacher} setIsTeacher= {setIsTeacher}/>
       <MainMinigamePage isTeacher= {isTeacher}/>
+      <MainMinigamePage/>
+      <Button1/>
+      <All/>
     </>
   );
 }
---
+++ my-react/src/Base/Auth.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Input, Button } from 'antd';
+import './Auth.css';
+
+const AuthExample = () => {
+const [passwordVisible, setPasswordVisible] = React.useState(false);
+
+return ( <div className="auth-container"> <div className="auth-form">
+
+
+    <Input
+      placeholder="Username"
+      className="username-input"
+    />
+
+    <div className="password-row">
+      <Input.Password
+        placeholder="Password"
+        visibilityToggle={{
+          visible: passwordVisible,
+          onVisibleChange: setPasswordVisible
+        }}
+        className="password-input"
+      />
+
+      <Button
+        className="show-button"
+        onClick={() => setPasswordVisible((prevState) => !prevState)}
+      >
+        {passwordVisible ? 'Hide' : 'Show'}
+      </Button>
+    </div>
+
+    <Button type="primary" className="enter-button">
+      Enter
+    </Button>
+
+  </div>
+</div>
+
+
+);
+};
+
+export default AuthExample;
